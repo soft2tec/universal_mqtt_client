c8dbeb4d45825fac413073868d58b3372844196a
Luca Casonato <luca.casonato@soft2tec.com>
2020-06-11 20:41:17 +0200

    More docs (#15)
    * Add an example and more documentation
    
    * Update readme
    
    * Update pubspec.yaml
    
    * Update README.md

--- example/simple_example.dart
+++ example/simple_example.dart
@@ -1,22 +1,3 @@
-# universal_mqtt_client
-
-[![Pub Version](https://img.shields.io/pub/v/universal_mqtt_client)](https://pub.dev/package/universal_mqtt_client)
-
-A MQTT client for Dart that works on any Dart supported compile target (including
-Flutter and Flutter Web).
-
-## Features
-
-- Full support for MQTT 3.3.1
-- Support for MQTT over WebSocket on all Dart compile targets (including web)
-- Support for MQTT over TCP on mobile and desktop
-- Dart idiomatic API using promises and streams
-- Built in reconnect functionality
-- Full support for wildcard topics
-
-## Usage
-
-```dart
 /// To use this example you need to have a WebSocket `mqtt` server
 /// running on localhost:9000.
 
@@ -53,15 +34,3 @@ void main() async {
   // Ultimatly we clean up our connection by disconnecting from the broker.
   client.disconnect();
 }
-```
-
-## Bugs and feature requests
-
-Please file feature requests and bugs at the [issue tracker][tracker]. If you find any security
-related issues, please follow the guidance in the `SECURITY.md` file. Thanks :-)
-
-[tracker]: https://github.com/soft2tec/universal_mqtt_client
-
-## Licence
-
-This project is licensed under the MIT licence. More information can be found in the `LICENCE` file.
